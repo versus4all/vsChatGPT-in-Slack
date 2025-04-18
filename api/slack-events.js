ddeca7e13b59c1c3d385c610c88f6dae2257e509
versus4all <versusall@gmail.com>
2025-04-18 16:09:09 +0400

    Update slack-events.js

--- api/slack-events.js
+++ api/slack-events.js
@@ -1,3 +1,4 @@
+import { pendingSummaries } from "./slack-interact";
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send("Method Not Allowed");
 
